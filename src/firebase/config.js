e66539fac8cca39a492bf1039c96d457fbe62914
kasprzyk <piotkas1@gmail.com>
2021-05-09 17:48:38 +0200

    firebase authentication setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC4MRR86eGw237mYXSdy3mImzwW9gHw_Vk',
@@ -7,13 +8,14 @@ const firebaseConfig = {
   projectId: 'new-project-2c64d',
   storageBucket: 'new-project-2c64d.appspot.com',
   messagingSenderId: '766454606253',
-  appId: '1:766454606253:web:1aa6f497fb2d6931de9924',
+  appId: '1:766454606253:web:1aa6f497fb2d6931de9924'
 };
 
 //init firebase
 firebase.initializeApp(firebaseConfig);
 
+const projectAuth = firebase.auth();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, timestamp };
+export { projectFirestore, timestamp, projectAuth };
