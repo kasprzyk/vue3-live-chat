2ae1b74fee8f111fbded3396e64785c00a7b9494
kasprzyk <piotkas1@gmail.com>
2021-05-09 18:03:05 +0200

    creating useSignup composable

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,30 +1,29 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <input
-      type="text"
-      required
-      placeholder="display name"
-      v-model="displayName"
-    />
-    <input type="email" required placeholder="email" v-model="email" />
-    <input type="password" required placeholder="password" v-model="password" />
+    <input v-model="displayName" type="text" required placeholder="display name" />
+    <input v-model="email" type="email" required placeholder="email" />
+    <input v-model="password" type="password" required placeholder="password" />
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue';
+import useSignup from '../composables/useSignup';
 export default {
   setup() {
     // refs
     const displayName = ref('');
     const email = ref('');
     const password = ref('');
-    const handleSubmit = () => {
-      console.log(displayName.value, email.value, password.value);
+    // useSignup
+    const { error, signup } = useSignup();
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      console.log('user signed up');
     };
     return { displayName, email, password, handleSubmit };
-  },
+  }
 };
 </script>
 
